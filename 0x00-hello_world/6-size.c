d2be9b19d1c59fc6d2e50d7ff3cb478b60302807
surafeal-dev <98810077+surafeal-dev@users.noreply.github.com>
2022-03-10 17:39:38 +0300

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-entry
+ * Return: when ended success
+ */
+int main()
+{
+        printf("size of a char:%d ", sizeof(char));
+        printf("size of an int:%d ",sizeof(int));
+        printf("size of a long:%d ",sizeof(long));
+         printf("size of a long long:%d ",sizeof(long long));
+        printf("size of a float:%d ",sizeof(float));
+        return 0;
+}
