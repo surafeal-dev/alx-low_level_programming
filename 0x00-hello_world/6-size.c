5ac208f8f6125eb21478459d0dd03e15da2b660e
surafeal-dev <98810077+surafeal-dev@users.noreply.github.com>
2022-03-10 18:14:29 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-        printf("size of a char:%d ", sizeof(char));
-                printf("size of an int:%d ", sizeof(int));
-                        printf("size of a long:%d ", sizeof(long));
-                                printf("size of a long long:%d ", sizeof(long long));
-                                        printf("size of a float:%d ", sizeof(float));
+printf("size of a char:%d ", sizeof(char));
+printf("size of an int:%d ", sizeof(int));
+printf("size of a long:%d ", sizeof(long));
+printf("size of a long long:%d ", sizeof(long long));
+printf("size of a float:%d ", sizeof(float));
 return (0);
 }
