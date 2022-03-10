b892a11058c500934b0e07e3546f14f1f60304de
surafeal-dev <98810077+surafeal-dev@users.noreply.github.com>
2022-03-10 16:40:09 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 *main- entry
 *Return- 0 when finished
 */
-int main()
+int main(void)
 {
-printf("%s","with proper grammar, but the outcome is a piece of art,");
+printf("%s", "with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
