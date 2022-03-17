9776aaa31e89dd129a8097db01f70be6fbaee505
surafeal-dev <98810077+surafeal-dev@users.noreply.github.com>
2022-03-18 00:26:10 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c + '0');
+	}
+
+	_putchar('\n');
+}
