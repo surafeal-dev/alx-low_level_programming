b5bf7e637fad593f2ecab45476199132bc3fcf8e
surafeal-dev <98810077+surafeal-dev@users.noreply.github.com>
2022-03-24 18:40:11 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,32 +1,27 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *leet - a function that encodes a string into 1337.
- *@s: string to be crypted .
- *Return: pointer to s .
- *Update V 1.0 : In this new code , i changed 2 arrays with 2 pointers, that
- *are pointed to the BASS adresses of the 2 arrays.
- *in addition to that , i assigned a pointer p to s.
+ * leet - Entry point
+ * ONE if, TWO loops only...
+ * @n: input
+ * Return: Always 0 (Success)
  */
-
-char *leet(char *s)
+char *leet(char *n)
 {
-	char *m = "aeotl";
-	char *M = "AEOTL";
-	int x[] = {'4', '3', '0', '7', '1'};
-	int i;
-	char *p = s;
+	int i, x;
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replacer[] = {'4', '3', '0', '7', '1'};
 
-	while (*s != '\0')
+	for (i = 0; n[i] != '\0'; i++)
 	{
-
-		for (i = 0; i < 5; i++)
+		for (x = 0; x <= 9; x++)
 		{
-			if (*s == *(m + i) || *s == *(M + i))
-
-				*s = x[i];
+			if (n[i] == find[x])
+			{
+				n[i] = replacer[x / 2];
+				x = 9;
+			}
 		}
-		s++;
-
 	}
-	return (p);
+
+	return (n);
 }
