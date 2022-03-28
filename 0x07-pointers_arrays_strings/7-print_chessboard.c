5213815a6bf1481d1f7e42be3f2104e3513cdba7
surafeal-dev <98810077+surafeal-dev@users.noreply.github.com>
2022-03-28 20:28:47 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,18 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_chessboard - prints chessboard.
- *
- * @a: rows.
- * Return: nothing.
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
 			_putchar(a[i][j]);
-		}
 		_putchar('\n');
 	}
 }
